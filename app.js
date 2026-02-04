28e79aba8dfb473fa112a5751012479f5778c51d
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-02-04 09:45:16 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+const methodOverride = require('method-override')
+const path = require("path");
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+
+const port= 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(() => {
+    console.log("Connected to DB");
+}).catch((err) => {
+    console.log(`Error : ${err}`);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+
+app.listen(port, () =>{
+    console.log(`Listening to port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hi");
+});
+
+//Index route
+app.get("/listings", async (req, res) => {
+    let allListings = await Listing.find();
+    res.render("listings/index.ejs", { allListings });
+});
+
+
+
+//New route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs")
+});
+
+//Create route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+
+
+//Show route(Read)
+app.get("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+
+
+//Edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+
+//Update route
+app.patch("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+
+
+
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(`Deleted : ${deletedListing}`);
+    res.redirect("/listings");
+});
---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default : "https://images.unsplash.com/photo-1647962431451-d0fdaf1cf21c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBzdW5zZXR8ZW58MHx8MHx8fDA%3D",
+        set: (v) =>
+            v === ""
+                ? "https://images.unsplash.com/photo-1647962431451-d0fdaf1cf21c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBzdW5zZXR8ZW58MHx8MHx8fDA%3D"
+                : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
