09c8d47e8990a754ec1da21c53e0f70131622011
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-02-23 13:16:01 +0530

    Signup error handling

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
 
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 }); 
 
+router.post("/signup", wrapAsync(async(req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({email, username});
+        const registeredUser = await User.register(newUser, password);
+        req.flash("success", `Welcome to wanderlust ${newUser.username}`);
+        res.redirect("/listings");
+    } catch (err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+}));
 
 module.exports = router;
