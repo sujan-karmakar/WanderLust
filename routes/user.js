80019037e50dde71cedfc88cc40632a407a22b5c
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-02-23 13:27:55 +0530

    User Login

--- routes/user.js
+++ routes/user.js
@@ -2,23 +2,41 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
-
+//SIGNUP
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
-}); 
+});
 
-router.post("/signup", wrapAsync(async(req, res) => {
-    try {
-        let { username, email, password } = req.body;
-        const newUser = new User({email, username});
-        const registeredUser = await User.register(newUser, password);
-        req.flash("success", `Welcome to wanderlust ${newUser.username}`);
+router.post(
+    "/signup",
+    wrapAsync(async (req, res) => {
+        try {
+            let { username, email, password } = req.body;
+            const newUser = new User({ email, username });
+            const registeredUser = await User.register(newUser, password);
+            req.flash("success", `Welcome to wanderlust ${newUser.username}`);
+            res.redirect("/listings");
+        } catch (err) {
+            req.flash("error", err.message);
+            res.redirect("/signup");
+        }
+    }),
+);
+
+//LOGIN
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }),
+    wrapAsync(async (req, res) => {
+        req.flash("success", "Welcome back to wanderlust!");
         res.redirect("/listings");
-    } catch (err) {
-        req.flash("error", err.message);
-        res.redirect("/signup");
-    }
-}));
+    }),
+);
 
 module.exports = router;
