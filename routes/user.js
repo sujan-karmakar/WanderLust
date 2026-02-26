9642c15ea81fe1c0fbd207d817bba4fc6fbb9f26
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-02-26 17:57:33 +0530

    MVC for user

--- routes/user.js
+++ routes/user.js
@@ -5,57 +5,27 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { savedRedirectUrl } = require("../middleware.js");
 
+const userController = require("../controllers/users.js");
+
 //SIGNUP
-router.get("/signup", (req, res) => {
-    res.render("users/signup.ejs");
-});
+router.get("/signup", userController.renderSignupForm);
 
 router.post(
     "/signup",
-    wrapAsync(async (req, res, next) => {
-        try {
-            let { username, email, password } = req.body;
-
-            const newUser = new User({ email, username });
-            const registeredUser = await User.register(newUser, password);
-
-            req.login(registeredUser, (err) => {
-                if(err) return next(err);
-                req.flash("success", `Welcome to wanderlust ${newUser.username}`);
-                res.redirect("/listings");
-            });
-        } catch (err) {
-            req.flash("error", err.message);
-            res.redirect("/signup");
-        }
-    }),
+    wrapAsync(userController.signup),
 );
 
 //LOGIN
-router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
-});
+router.get("/login", userController.renderLoginForm);
 
 router.post(
     "/login",
     savedRedirectUrl,
     passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }),
-    wrapAsync(async (req, res) => {
-        req.flash("success", "Welcome back to wanderlust!");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-    }),
+    wrapAsync(userController.login),
 );
 
-router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if(err) {
-            next(err);
-        }
-        req.flash("success", "you are logged out now");
-        res.redirect("/listings");
-    });
-}); 
+router.get("/logout", userController.logout); 
 
 
 module.exports = router;
