d25387a00918e42cd4af9aa0a6f9387397ff545d
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-03-01 17:34:07 +0530

    Coordinates of listings added

--- public/js/map.js
+++ public/js/map.js
@@ -3,12 +3,13 @@ maptilersdk.config.apiKey = mapToken;
 const map = new maptilersdk.Map({
     container: "map", // container's id or the HTML element in which the SDK will render the map
     style: maptilersdk.MapStyle.STREETS,
-    center: coordinates, // starting position [lng, lat]
+    center: listing.geometry.coordinates, // starting position [lng, lat]
     zoom: 10, // starting zoom
 });
 
 
 const marker = new maptilersdk.Marker({
 color: "red",
-}).setLngLat(coordinates)
+}).setLngLat(listing.geometry.coordinates)
+.setPopup(new maptilersdk.Popup().setHTML(`<h5>${listing.title}</h5><br><h6>Exact location provided after booking</h6>`))
 .addTo(map);
