9427b119cb2079ae147636698d4ee4df8da46ac1
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-03-02 12:24:12 +0530

    Category added during listing creation

--- schema.js
+++ schema.js
@@ -7,7 +7,8 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null)
+        image: Joi.string().allow("", null),
+        categories: Joi.array().items(Joi.string().valid("Trending", "Rooms", "Iconic Cities", "Mountains", "Camping", "Castles", "Amazing Pools", "Farms", "Arctic", "Beach")).optional()
     }).required()
 });
 
